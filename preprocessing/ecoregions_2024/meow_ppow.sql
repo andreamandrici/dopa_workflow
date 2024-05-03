d2651aa1514ef90b74313ef0690e849434b7297d
Andrea <andreamandrici@users.noreply.github.com>
2024-05-03 22:06:44 +0200

    Update meow_ppow.sql

--- preprocessing/ecoregions_2024/meow_ppow.sql
+++ preprocessing/ecoregions_2024/meow_ppow.sql
@@ -66,17 +66,50 @@ FROM a NATURAL JOIN b
 ORDER BY source,third_level_code,second_level_code,first_level_code;
 INSERT INTO ppow1 SELECT * FROM ppow2;
 
+DROP TABLE IF EXISTS ppow_correction3;CREATE TEMPORARY TABLE ppow_correction3 AS
+WITH
+a AS (
 SELECT * FROM habitats_and_biotopes.meow_ppow_1
 WHERE source = 'ppow'
---AND realm ILIKE 'Atlantic Warm Water%'
---AND biome ILIKE 'Transitional%'
---AND provinc ILIKE 'South Central%'
-AND provinc NOT IN (SELECT DISTINCT provinc FROM ppow1);
-
+AND realm = 'Indo-Pacific Warm Water'
+AND biome = 'Boundary - western'
+AND provinc = 'Agulhas Current'
+AND provinc NOT IN (SELECT DISTINCT provinc FROM ppow1)
+),
+b AS (
 SELECT * FROM ppow_correction
-WHERE 
---third_level = 'Indo-Pacific Warm Water' AND 
-second_level = 'Gyre'
-AND first_level ILIKE 'South Central%';
+WHERE third_level = 'Indo-Pacific Warm Water'
+AND second_level = 'Western Boundary'
+AND first_level = 'Agulhas Current')
+SELECT
+a.source,third_level_code,realm third_level,
+second_level_code,biome second_level,
+first_level_code,provinc first_level
+FROM a,b;
 
+DROP TABLE IF EXISTS ppow3;CREATE TEMPORARY TABLE ppow3 AS
+WITH
+a AS (
+SELECT *,provinc first_level,biome second_level,realm third_level
+FROM habitats_and_biotopes.meow_ppow_1 WHERE source = 'ppow' AND provinc NOT IN (SELECT DISTINCT provinc FROM ppow1)
+),
+b AS (
+SELECT first_level_code,first_level,second_level_code,second_level,third_level_code,third_level
+FROM ppow_correction3)
+SELECT source,third_level_code,realm,second_level_code,biome,first_level_code,provinc
+FROM a NATURAL JOIN b
+ORDER BY source,third_level_code,second_level_code,first_level_code;
+INSERT INTO ppow1 SELECT * FROM ppow3;
 
+--------------------------------------------------
+DROP TABLE IF EXISTS habitats_and_biotopes.meow;CREATE TABLE habitats_and_biotopes.meow AS
+SELECT * FROM meow1 a 
+NATURAL JOIN habitats_and_biotopes.meow_ppow_1 b
+ORDER BY source,third_level_code,second_level_code,first_level_code;
+
+DROP TABLE IF EXISTS habitats_and_biotopes.ppow;CREATE TABLE habitats_and_biotopes.ppow AS
+SELECT * FROM ppow1 a 
+NATURAL JOIN habitats_and_biotopes.meow_ppow_1 b
+ORDER BY source,third_level_code,second_level_code,first_level_code;
+
+DROP TABLE IF EXISTS habitats_and_biotopes.meow_ppow_1;
