d18764c6354fe5104829247b4d73c49c1822b836
Andrea <andreamandrici@users.noreply.github.com>
2024-10-30 17:37:42 +0100

    Update postprocessing.sql

--- preprocessing/admin_2020/postprocessing.sql
+++ preprocessing/admin_2020/postprocessing.sql
@@ -9,7 +9,6 @@ SELECT * FROM gisco_2020_flat.fb_atts_all WHERE NOT (land && '{0}' OR marine && 
 SELECT * FROM gisco_2020_flat.fb_atts_all WHERE NOT (land && '{0}') AND NOT (abnj && '{0}'); -- overlap land and abnj;
 SELECT * FROM gisco_2020_flat.fb_atts_all WHERE NOT (marine && '{0}') AND NOT (abnj && '{0}'); -- overlap marine and abnj;
 
-
 DROP TABLE IF EXISTS problems;CREATE TEMPORARY TABLE problems AS
 WITH
 gisco_flat AS (SELECT DISTINCT cid,l,m,a,g FROM gisco_2020_flat.h_flat,UNNEST(land,marine,abnj,grid) AS u(l,m,a,g) ORDER BY cid),
@@ -25,12 +24,32 @@ UNION
 SELECT *,5 ido,'marine-abnj overlap' deso FROM gisco_flat WHERE m!=0 AND a!=0
 ),
 less_problems AS (SELECT DISTINCT cid,ido,deso FROM problems ORDER BY cid,ido)
-SELECT *,NULL::text final_class FROM gisco_2020_flat.fb_atts_all LEFT JOIN less_problems USING(cid) ORDER BY cid;
+SELECT *,NULL::integer final_class FROM gisco_2020_flat.fb_atts_all LEFT JOIN less_problems USING(cid) ORDER BY cid;
 
+SELECT * FROM problems
+WHERE ido IS NULL AND final_class IS NULL AND NOT (land && '{0}')
 
+UPDATE problems SET final_class=a.final_class
+FROM (SELECT cid,UNNEST(land) final_class FROM problems WHERE ido IS NULL AND final_class IS NULL AND NOT (land && '{0}')) a
+WHERE problems.cid=a.cid;
+
+SELECT * FROM problems
+WHERE ido IS NULL AND final_class IS NULL AND NOT (marine && '{0}')
+
+UPDATE problems SET final_class=a.final_class
+FROM (SELECT cid,UNNEST(marine) final_class FROM problems WHERE ido IS NULL AND final_class IS NULL AND NOT (marine && '{0}')) a
+WHERE problems.cid=a.cid;
+
+SELECT * FROM problems
+WHERE ido IS NULL AND final_class IS NULL AND NOT (abnj && '{0}')
+
+UPDATE problems SET final_class=a.final_class
+FROM (SELECT cid,1000+UNNEST(abnj) final_class FROM problems WHERE ido IS NULL AND final_class IS NULL AND NOT (abnj && '{0}')) a
+WHERE problems.cid=a.cid;
+
+	
 DROP TABLE IF EXISTS gisco_2020.gisco_flat1;CREATE TABLE gisco_2020.gisco_flat1 AS
 SELECT a.*,b.ido,b.deso,b.final_class FROM gisco_2020_flat.h_flat a JOIN problems b USING(cid) ORDER BY qid,cid;
 
 DROP TABLE IF EXISTS gisco_2020.gisco_flat1_atts;CREATE TABLE gisco_2020.gisco_flat1_atts AS
 SELECT * FROM problems ORDER BY cid;
-
